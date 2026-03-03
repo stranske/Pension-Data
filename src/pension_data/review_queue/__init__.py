7c40da19f53617c1c0b731b0715e1d9f1e92dc25
stranske <stranske@gmail.com>
2026-03-02 20:52:52 -0600

    Issue #30: add entity review queue workflow with audited decisions (#96)
    * Issue #30: add entity review queue workflow and audits
    
    * Validate runtime decision actions in entity review workflow

--- src/pension_data/review_queue/__init__.py
+++ src/pension_data/review_queue/__init__.py
@@ -1,6 +1,12 @@
 """Review queue routing helpers."""
 
 from pension_data.review_queue.anomalies import ReviewQueueItem, route_anomalies_to_review_queue
+from pension_data.review_queue.entities import (
+    apply_entity_review_decision,
+    ingest_entity_review_candidates,
+    list_pending_entity_reviews,
+    list_resolved_entity_reviews,
+)
 from pension_data.review_queue.extraction import (
     build_extraction_review_queue,
     transition_extraction_review_state,
@@ -8,7 +14,11 @@ from pension_data.review_queue.extraction import (
 
 __all__ = [
     "ReviewQueueItem",
+    "apply_entity_review_decision",
     "build_extraction_review_queue",
+    "ingest_entity_review_candidates",
+    "list_pending_entity_reviews",
+    "list_resolved_entity_reviews",
     "route_anomalies_to_review_queue",
     "transition_extraction_review_state",
 ]
