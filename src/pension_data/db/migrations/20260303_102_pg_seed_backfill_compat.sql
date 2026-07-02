e72edccdc09e1256798b77f11829f5f6b6aa140d
Tim Stranske <stranske@gmail.com>
2026-07-02 10:31:39 -0500

    Fix PostgreSQL bitemporal backfill

--- src/pension_data/db/migrations/20260303_102_pg_seed_backfill_compat.sql
+++ src/pension_data/db/migrations/20260303_102_pg_seed_backfill_compat.sql
@@ -5,13 +5,9 @@ UPDATE staging_core_metrics
 SET
   plan_period = COALESCE(NULLIF(plan_period, ''), 'UNKNOWN'),
   benchmark_version = COALESCE(NULLIF(benchmark_version, ''), 'v1'),
-  ingestion_date = COALESCE(NULLIF(ingestion_date, ''), effective_date),
-  valid_from = COALESCE(NULLIF(valid_from, ''), effective_date),
-  asserted_at = COALESCE(
-    NULLIF(asserted_at, ''),
-    NULLIF(ingestion_date, ''),
-    effective_date
-  ),
+  ingestion_date = COALESCE(ingestion_date, effective_date),
+  valid_from = COALESCE(valid_from, effective_date),
+  asserted_at = COALESCE(asserted_at, ingestion_date, effective_date),
   restated = superseded_at IS NOT NULL
 WHERE
   plan_period IS NULL
@@ -19,11 +15,8 @@ WHERE
   OR benchmark_version IS NULL
   OR benchmark_version = ''
   OR ingestion_date IS NULL
-  OR ingestion_date = ''
   OR valid_from IS NULL
-  OR valid_from = ''
   OR asserted_at IS NULL
-  OR asserted_at = ''
   OR (
     superseded_at IS NOT NULL
     AND restated IS DISTINCT FROM TRUE
